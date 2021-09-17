f92b15bc8cee744a69e10fa513518ee29a5906aa
Aprea <apreak@gmail.com>
2021-09-17 19:44:46 +0700

    addfiles

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AreRichApp extends StatelessWidget {
+  const AreRichApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Container(),
+    );
+  }
+}
